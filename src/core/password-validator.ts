ade964bf32234cb9580e7da3e5c4822ebad61101
acaleza <adrian.calvo1@gmail.com>
2023-04-25 00:07:58 +0200

    refactor: The password doesn't contain a number

--- src/core/password-validator.ts
+++ src/core/password-validator.ts
@@ -1,5 +1,14 @@
 const MINIMUM_LENGTH = 6;
 const regExp_numbers = /\d/;
+
 export function isStrongEnough(password: string) {
-    return password.length>=MINIMUM_LENGTH && regExp_numbers.test(password);
+    return hasMinimumLength(password) && containNumbers(password);
+}
+
+function hasMinimumLength(password: string) {
+    return password.length >= MINIMUM_LENGTH;
+}
+
+function containNumbers(password: string) {
+    return regExp_numbers.test(password);
 }
