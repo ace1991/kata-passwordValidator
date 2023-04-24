1eaf3445b8ebbacc881500c6b41c19bad7e30a9d
acaleza <adrian.calvo1@gmail.com>
2023-04-25 00:05:03 +0200

    green: The password doesn't contain a number

--- src/core/password-validator.ts
+++ src/core/password-validator.ts
@@ -1,5 +1,5 @@
 const MINIMUM_LENGTH = 6;
-
+const regExp_numbers = /\d/;
 export function isStrongEnough(password: string) {
-    return password.length>=MINIMUM_LENGTH;
+    return password.length>=MINIMUM_LENGTH && regExp_numbers.test(password);
 }
